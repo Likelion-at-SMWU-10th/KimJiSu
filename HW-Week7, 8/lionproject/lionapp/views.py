ba57dbe52d00471f564f155693da310dccce7d7b
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-07-12 18:11:52 +0900

    [Fix] 하얀 화살표

---
+++ HW-Week7, 8/lionproject/lionapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'lionapp/home.html')
+
+def blog(request):
+    blogs = Blog.objects
+    return render(request, 'likeapp/blog.html',{'blogs' : blogs })
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'lionapp/detail.html', {'blog': blog_detail})
